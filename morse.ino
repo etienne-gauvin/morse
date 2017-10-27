b3da5af4d9f016a0541969675ac197a6d18609af
Étienne Gauvin <etienne@gauvin.info>
2017-10-27 12:14:22 +0200

    Added comments and reorganized the code

--- morse.ino
+++ morse.ino
@@ -2,39 +2,36 @@
 #include "morseencrypt.h"
 #include "morsetransmit.h"
 
-char stringSentence[] = "NEVER GONNA LET YOU UP NEVER GONNA LET YOU DOWN NEVER GONNA RUN AROUND AND DESERT YOU";
-MorseSentence sentence;
-MorseTransmission transmission;
-
 unsigned long time = 0;
 unsigned long lastTime = 0;
 
+MorseSentence sentence;
+MorseTransmission transmission;
+
+// The sentence to transmit
+char stringSentence[] = "NEVER GONNA LET YOU UP NEVER GONNA LET YOU DOWN NEVER GONNA RUN AROUND AND DESERT YOU";
+
 void setup() {
   Serial.begin(9600);
-
-  sentence.from(stringSentence, sizeof(stringSentence) - 1);
-
-  transmission.sentence = & sentence;
-
-  transmission.timeUnit = 250;
   
+  // Init the sentence
+  sentence.from(stringSentence, sizeof(stringSentence) - 1);
+  
+  // Init the transmission
+  transmission.sentence = & sentence;
+  transmission.timeUnit = 250;
   transmission.dashOutputPin = LED_BUILTIN;
   transmission.dotOutputPin = LED_BUILTIN;
-
   transmission.init();
-  
-  Serial.println();
 }
 
 void loop() {
 
   time = millis();
-
   if (time < lastTime) {
     lastTime = 0;
   } else {
     transmission.loop(time - lastTime);
-    
     lastTime = time;  
   }
 
